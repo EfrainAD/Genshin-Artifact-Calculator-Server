2e9a7bffafe1740884bb85a6484795ee95d4a21b
f-eiyu <feiyu@alum.mit.edu>
2022-08-04 11:25:34 +0200

    Create artifact schema and set up architecture
    The back end calculations will be very data intensive, and so several files here were set up
    in order to store all of the probability and numberical data that will be used in these
    computations later on.

--- app/models/example.js
+++ app/models/example.js
@@ -1,24 +1,45 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const { nameToSet } = require('../utils/data/name-to-set');
 
-const exampleSchema = new mongoose.Schema(
-	{
-		title: {
-			type: String,
-			required: true,
-		},
-		text: {
-			type: String,
-			required: true,
-		},
-		owner: {
-			type: mongoose.Schema.Types.ObjectId,
-			ref: 'User',
-			required: true,
-		},
+const artifactSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true
 	},
-	{
-		timestamps: true,
+	level: {
+		type: Number,
+		required: true
+	},
+	mainStat: {
+		type: String,
+		required: true
+	},
+	mainStatAmount: {
+		type: Number,
+		required: true
+	},
+	substats: [{
+		stat: {
+			type: String,
+			required: true
+		},
+		amount: {
+			type: Number,
+			required: true
+		}
+	}],
+	owner: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "User",
+		required: true
 	}
-)
+}, {
+	timestamps: true,
+});
 
-module.exports = mongoose.model('Example', exampleSchema)
+// find the artifact's set from its proper name
+artifactSchema.virtual("set").get(function() {
+	return nameToSet(this.name);
+});
+
+module.exports = mongoose.model("Artifact", artifactSchema);
---
+++ app/utils/data/name-to-set.js
@@ -0,0 +1,6 @@
+// takes in the name of an artifact and returns the set that it belongs to
+const nameToSet = (name) => {
+  console.log("hi", name);
+}
+
+module.exports = { nameToSet };
